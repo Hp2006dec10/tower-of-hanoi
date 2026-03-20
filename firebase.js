821e2cd1d6f716b1110aab28fe0c5ca9e82a92db
Hariprasad <hariragul2006@gmail.com>
2026-03-20 21:37:24 +0530

    Added safety measures for API key

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5r3--q4Y-R2yMmRtzEdK9Q3sfy7J7xFo",
-  authDomain: "adavya-tower-of-hanoi.firebaseapp.com",
-  projectId: "adavya-tower-of-hanoi",
-  storageBucket: "adavya-tower-of-hanoi.firebasestorage.app",
-  messagingSenderId: "363857810596",
-  appId: "1:363857810596:web:b5fab7950938013ea90231",
-  measurementId: "G-HB2REPJJMD"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
